6855ca0bbcd26a484ab182375d5123538ec07a44
Mitchel3014 <49696041+Mitchel3014@users.noreply.github.com>
2020-11-23 23:18:29 +0300

    Repeating lessons. New version of Homework_7

--- Java_1_Level_1_Lesson_7_HW_New.java
+++ Java_1_Level_1_Lesson_7_HW_New.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * Java. Level 1. Lesson 6. Homework 5 New version
+ * Java. Level 1. Lesson 7. Homework 7 New version
  * @author Artem Fomin
  * @version dated 06.11.2020
  */
@@ -17,13 +17,15 @@ public class Java_1_Level_1_Lesson_7_HW_New {
         cats [3] = new GCat("Рыжий", 10, 100);
         Plate plate = new Plate (0);
 
-        System.out.println(plate);
+        System.out.println(plate);//Выводим информацию о тарелке
+        /*Проходим по массиву котов и просим каждого поесть из тарелки*/
         for (int i = 0; i < cats.length; i++) {
+            /*Проверка на отказ пользователся класть еду*/
             if (cats [i].eat(plate) == 2) {
                 break;
             }
         }
-        System.out.println(plate);
+        System.out.println(plate);//Выводим информацию о тарелке
     }
 }
 
@@ -37,32 +39,46 @@ class GCat {
     //Создаем конструктор суперкласса
     GCat (String name, int appetite, int satiety) {
         this.name = name;
-        this.appetite = appetite;
-        this.satiety = satiety;
+        this.appetite = appetite;//Аппетит
+        this.satiety = satiety;//Сытость
+        /*Счетчик насыщения кота (при полном насыщении кота
+        должен быть равен заданному параметру сытости)*/
         this.fullness = 0;
     }
 
     //Создаем метод с помощью котрого кот ест
     public int eat (Plate p) {
         Scanner scanner = new Scanner(System.in);
+        /*Создаем контролирующий цикл, работающий до тех пор, пока коты голодны,
+        т.е. коты едят только пока они голодные*/
         while (fullness != satiety) {
+            /*Условие для проверки достаточности еды в тарелке. Кот не ест, если в тарелке еды меньше,
+            чем его аппетит*/
             if (p.getFood() >= appetite) {
-                p.decreaseFood(appetite);
-                fullness = fullness + appetite;
+                p.decreaseFood(appetite);//Вывзов метода уменьшения колчества еды в тарелке
+                fullness = fullness + appetite;//Увеличение счетчика насыщения кота
+                /*Условие проверки сытости кота и действий при ее достижении*/
                 if (fullness == satiety) {
                     System.out.println("Кот " + name + " наелся.\n" +
                             "Сытость кота: " + fullness + " из " + satiety);
                     break;
                 }
+                /*Проверка на то, что кот все еще голоден и набор дальнейших действий*/
             } else if (p.getFood() < appetite) {
                 System.out.println("Недостаточно еды! Кот " + name + " голоден.\n" +
                         "Сытость кота: " + fullness + " из " + satiety +
                         "\nЕда в тарелке: " + p.getFood());
+                /*Спрашиваем пользователя, хотим ли мы добавить еду в тарелку*/
                 System.out.println("Еды в тарелке недостаточно! Положить котам еще еды: да - 1; нет - 2?");
+                /*Если пользователь согласен добавить еду, спашиваем, сколько он хочет добавить*/
                 if (scanner.nextInt() == 1) {
                     System.out.println("Сколько еды положить?");
+                    /*Вызываем метод добавления еды и в качестве параметра передаем значение,
+                    которое ввел пользователь*/
                     p.addFood(scanner.nextInt());
                 } else {
+                    /*Если пользователь не захотел ничего добавлять, выводим об этом сообщене,
+                    закрываем сканнер и выходим из цикла*/
                     System.out.println("Вы не захотели класть еду!");
                     scanner.close();
                     return 2;
